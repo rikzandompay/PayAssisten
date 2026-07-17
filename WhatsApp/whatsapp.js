95194e8cc95f7aaf83c1f827d697309fbe7326c1
rikzandompay <dioanhar9@gmail.com>
2026-07-17 21:19:33 +0700

    fix: update whatsapp-web.js to latest and add webVersionCache

--- WhatsApp/whatsapp.js
+++ WhatsApp/whatsapp.js
@@ -43,7 +43,11 @@ class WhatsAppService {
                         clientId: this.tenantId,
                         dataPath: '.wwebjs_auth'
                     }),
-                    puppeteer
+                    puppeteer,
+                    webVersionCache: {
+                        type: 'remote',
+                        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+                    }
                 });
 
                 this.registerEvents();
